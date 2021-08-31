499f82f55939e66416f506b5b7a51064e09abb6e
Yichong Xu <yicxu@microsoft.com>
2021-08-31 11:05:19 -0700

    update config settings

--- bash/task_train.sh
+++ bash/task_train.sh
@@ -4,5 +4,5 @@ export DATA_DIR="data/"
 export OUTPUT_DIR="output/"
 
 
-python task.py --append_answer_text 1 --batch_size 8 --append_descr 1 --max_seq_length 192 --test_mode
+python task.py --append_answer_text 1 --batch_size 8 --append_descr 1 --max_seq_length 192 --lr 1e-5 --weight_decay 0.15
 
