1e9e66feb745bc156777fdc8d2bebc754a6cd7c0
Yichong Xu <xuyc11@gmail.com>
2022-04-09 15:52:56 -0700

    Update task_train.sh

--- bash/task_train.sh
+++ bash/task_train.sh
@@ -14,3 +14,6 @@ python -m torch.distributed.launch --nproc_per_node=2 task.py --append_descr 1 -
 
 # train a deberta v2 xlarge (900M) model using deepspeed, testing the pipeline
 deepspeed task.py --append_descr 1 --append_triples --append_retrieval 1 --data_version csqa_ret_3datasets --lr 5e-6 --append_answer_text 1 --weight_decay 0 --preset_model_type debertav2-xlarge --batch_size 1 --max_seq_length 50 --num_train_epochs 15 --save_interval_step 4 --continue_train --print_number_per_epoch 1 --vary_segment_id --seed 42 --warmup_proportion 0.1 --optimizer_type adamw --ddp --deepspeed --test_mode --clear_output_folder
+
+# reproduce results of DeBERTa v3 in paper
+deepspeed task.py --append_descr 1 --append_triples --append_retrieval 1 --data_version csqa_ret_3datasets --lr 4e-6 --append_answer_text 1 --weight_decay 0.1 --preset_model_type debertav3 --batch_size 4 --max_seq_length 512 --num_train_epochs 15 --save_interval_step 4 --continue_train --print_number_per_epoch 1 --vary_segment_id --seed 42 --warmup_proportion 0.1 --optimizer_type adamw --ddp --deepspeed --freq_rel 1 
